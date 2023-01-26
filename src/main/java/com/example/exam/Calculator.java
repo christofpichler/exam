e46a157cf83027e7602aede79efc2c920f91f248
christofpichler <christof.pichler@homepichler.net>
2023-01-26 18:53:48 +0100

    merge into master

--- src/main/java/com/example/exam/Calculator.java
+++ src/main/java/com/example/exam/Calculator.java
@@ -5,14 +5,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Calculator {
-
+    //Abfrage
     @GetMapping ("/calc")
     public String inchConverter(String entry)
     {
         try{
             double inch = Double.valueOf(entry);
+            // Umrechnung inch in cm
             return String.valueOf(inch * 2.54);
         }catch (Exception e){
+            //Ausgabe bei Fehlerhaften Parametern
             return "Wert falsch!";
         }
 
